c927133e9a37a6d78838933d26940db3f19424c5
ARAFAT <mdarafathossen.py@gmail.com>
2026-02-03 15:25:33 +0600

    SSLCommarz Payment API Added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
-from order.views import CartViewSet, CartItemViewSet, OrderViewSet
+from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', include(cart_router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('payment/initiate/', initiate_payment, name='initiate-payment'),
 
 ]
--- order/views.py
+++ order/views.py
@@ -4,10 +4,11 @@ from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyM
 from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer, OrderSerializer, CreateOrderSerializer, UpdateOrderSerializer, EmptySerializer
 from order.models import Cart, CartItem, Order, OrderItem
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from order.services import OrderService
 from rest_framework.response import Response
 from rest_framework import status
+from sslcommerz_lib import SSLCOMMERZ
 
 # Create your views here.
 
@@ -100,3 +101,40 @@ class OrderViewSet(ModelViewSet):
         return Order.objects.prefetch_related('items__product').filter(user=self.request.user)
 
 
+@api_view(['POST'])
+def initiate_payment(request):
+    user = request.user
+    amount = request.data.get('amount')
+    order_id = request.data.get('orderId')
+    num_items = request.data.get('numItems')
+
+    settings = { 'store_id': 'phima6981740f6f27d', 'store_pass': 'phima6981740f6f27d@ssl', 'issandbox': True }
+    sslcz = SSLCOMMERZ(settings)
+    post_body = {}
+    post_body['total_amount'] = amount
+    post_body['currency'] = "BDT"
+    post_body['tran_id'] = f"trxn_{order_id}"
+    post_body['success_url'] = "http://localhost:5173/dashboard/payment/success/"
+    post_body['fail_url'] = "http://localhost:5173/dashboard/payment/fail/"
+    post_body['cancel_url'] = "http://localhost:5173/dashboard/orders/"
+    post_body['emi_option'] = 0
+    post_body['cus_name'] = f"{user.first_name} {user.last_name}"
+    post_body['cus_email'] = user.email
+    post_body['cus_phone'] = user.phone_number
+    post_body['cus_add1'] = user.address
+    post_body['cus_city'] = "Dhaka"
+    post_body['cus_country'] = "Bangladesh"
+    post_body['shipping_method'] = "Curier"
+    post_body['multi_card_name'] = ""
+    post_body['num_of_item'] = num_items
+    post_body['product_name'] = "E-commerce Products"
+    post_body['product_category'] = "General"
+    post_body['product_profile'] = "general"
+
+
+    response = sslcz.createSession(post_body) # API response
+    
+    if response.get('status') == 'SUCCESS':
+        return Response({'payment_url': response.get('GatewayPageURL')})
+    
+    return Response({'error': 'Failed to initiate payment'}, status=status.HTTP_400_BAD_REQUEST)
