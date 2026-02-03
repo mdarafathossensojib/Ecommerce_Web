c4fd13b18ae3242d76b108583df0f0e11c19e1ba
ARAFAT <mdarafathossen.py@gmail.com>
2026-02-03 22:44:22 +0600

    SSL Commarz Fixed

--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer, OrderSerializer, CreateOrderSerializer, UpdateOrderSerializer, EmptySerializer
@@ -10,6 +10,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from sslcommerz_lib import SSLCOMMERZ
 from decouple import config
+from django.conf import settings as main_settings
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -102,6 +104,7 @@ class OrderViewSet(ModelViewSet):
         return Order.objects.prefetch_related('items__product').filter(user=self.request.user)
 
 
+
 @api_view(['POST'])
 def initiate_payment(request):
     user = request.user
@@ -115,9 +118,9 @@ def initiate_payment(request):
     post_body['total_amount'] = amount
     post_body['currency'] = "BDT"
     post_body['tran_id'] = f"trxn_{order_id}"
-    post_body['success_url'] = "http://localhost:5173/dashboard/payment/success/"
-    post_body['fail_url'] = "http://localhost:5173/dashboard/payment/fail/"
-    post_body['cancel_url'] = "http://localhost:5173/dashboard/orders/"
+    post_body['success_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/success/"
+    post_body['fail_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/fail/"
+    post_body['cancel_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/cancel/"
     post_body['emi_option'] = 0
     post_body['cus_name'] = f"{user.first_name} {user.last_name}"
     post_body['cus_email'] = user.email
@@ -139,3 +142,22 @@ def initiate_payment(request):
         return Response({'payment_url': response.get('GatewayPageURL')})
     
     return Response({'error': 'Failed to initiate payment'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['POST'])
+def payment_success(request):
+    order_id = request.data.get('tran_id').split('_')[1]
+    order = Order.objects.get(id=order_id)
+    order.status = 'Ready To Ship'
+    order.save()
+
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
+
+@api_view(['POST'])
+def payment_cancel(request):
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
+
+@api_view(['POST'])
+def payment_fail(request):
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
